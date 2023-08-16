4a1f1708c43ca40461209590f66d9b0989e96cd8
Zhen Yang <webyangfullstack@gmail.com>
2023-08-17 03:57:37 +1000

    build experience and skills section

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -4,14 +4,40 @@ import { BsBriefcase } from 'react-icons/bs'
 
 const Resume = () => {
   return (
-    <div className='w-full h-screen text-white pt-[70px]' id='resume'>
+    <div className='w-full text-white pt-[70px]' id='resume'>
       <div className='w-[136px] h-[35px] border-[1px] border-[#565656] rounded-full text-white flex justify-center items-center gap-2 text-xs mb-[58px]'>
         <BsBriefcase className="mb-[2px] text-sm" />
         <span className='uppercase font-[250]'>Resume</span>
       </div>
       {/* title section */}
-      <div className=''>
+      <div className='cursor-default'>
         <h3 className='text-5xl leading-[60px] font-light mb-8'>Education &<span className='text-[#58EA8B]'> Experience</span></h3>
+        <section className='resume-container flex flex-col ml-2'>
+          {/* 2023-present */}
+          <div className='resume-item relative pl-[74px] pb-[68px] text-[#999999] hover:text-[#28e98c] transition-all duration-[0.3] ease-in-out'>
+            <div className='text-base pb-[28px] cursor-default'>2023 - Present</div>
+            <h4 className='text-white text-2xl mb-[5px]'>Fullstack Developer</h4>
+            <p className='text-[#999999] text-xs mb-[18px]'>BCR</p>
+            <h4 className='text-white text-2xl mb-[5px]'>Fullstack Developer</h4>
+            <p className='text-[#999999] text-xs'>Edge Ark</p>
+          </div>
+          {/* 2020-2022 */}
+          <div className='resume-item relative pl-[74px] pb-[68px] text-[#999999] hover:text-[#28e98c] transition-all duration-[0.3] ease-in-out'>
+            <div className='text-base pb-[28px] cursor-default'>2020-2022</div>
+            <h4 className='text-white text-2xl mb-[5px]'>Frontend Developer</h4>
+            <p className='text-[#999999] text-xs mb-[18px]'>BCU World</p>
+            <h4 className='text-white text-2xl mb-[5px]'>Fullstack Developer</h4>
+            <p className='text-[#999999] text-xs'>JR Academy</p>
+          </div>
+          {/* 2012-2020 */}
+          <div className='resume-item relative pl-[74px]  text-[#999999] hover:text-[#28e98c] transition-all duration-[0.3] ease-in-out'>
+            <div className='text-base pb-[28px] cursor-default'>2012-2020</div>
+            <h4 className='text-white text-2xl mb-[5px]'>Master of Science in Internetworking</h4>
+            <p className='text-[#999999] text-xs mb-[18px]'>UTS</p>
+            <h4 className='text-white text-2xl mb-[5px]'>Bachelor of Computer science in Gaming</h4>
+            <p className='text-[#999999] text-xs'>Macao Polytechnic University</p>
+          </div>
+        </section>
       </div>
     </div>
   )
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,13 +1,29 @@
 'use client'
 import React from 'react'
 import { LiaShapesSolid } from 'react-icons/lia'
+import { skills } from '@/utils/constants'
 
 const Skills = () => {
     return (
-        <div className='w-full h-screen text-white pt-[70px]' id='skills'>
+        <div className='w-full text-white pt-[70px]' id='skills'>
             <div className='w-[136px] h-[35px] border-[1px] border-[#565656] rounded-full text-white flex justify-center items-center gap-2 text-xs mb-[58px]'>
                 <LiaShapesSolid className="mb-[2px] text-sm" />
                 <span className='uppercase font-[250]'>Skills</span>
+            </div>
+            <div className='cursor-default'>
+                <h3 className='text-5xl leading-[60px] font-light mb-8'>My<span className='text-[#58EA8B]'> TechStack</span></h3>
+            </div>
+            <div className='w-full flex flex-row flex-wrap gap-5'>
+                {skills.map((skill, index) => (
+                    // <div key={index} className='w-[calc(50%-10px)] h-[calc(50%-10px)] bg-[#1f1f1f] rounded-3xl flex flex-col justify-center items-center gap-2 p-5'>
+                    //     <skill.icon className='text-[#58EA8B] text-6xl' />
+                    //     <span className='text-[#999999] text-lg'>{skill.title}</span>
+                    // </div>
+                    <div key={index} className='w-[100px] h-[100px] border-[2px] border-[#999999] hover:border-[#58EA8B] hover:bg-[#1f1f1f] rounded-3xl flex flex-col justify-center items-center gap-2 p-1 cursor-pointer transition-all duration-[0.3] ease-in-out text-[#999999] hover:text-white'>
+                        <skill.icon className='text-[#58EA8B] text-3xl' />
+                        <span className='text-sm text-center'>{skill.title}</span>
+                    </div>
+                ))}
             </div>
         </div>
     )
