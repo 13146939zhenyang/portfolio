5f87ff54b6ec921eb407e41e77a0d8c6ceff8645
Zhen Yang <webyangfullstack@gmail.com>
2023-08-21 03:18:38 +1000

    update app structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useRef, useEffect, useState } from 'react'
 import { Introduction, About, Contact, DeveloperCard, Portfolio, Resume, Skills, SideNav } from '@/components'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   const scrollRef = useRef(null)
@@ -15,36 +15,34 @@ export default function Home() {
     }
   }
   return (
-    <main className='w-full h-full relative'>
-      <div className='absolute top-0 left-0 w-full h-full overflow-hidden'>
+    <main className='w-full h-full relative overflow-scroll'>
+      <div className='absolute top-0 left-0 w-full h-full overflow-hidden -z-30'>
         <video autoPlay muted loop className="w-full h-full object-cover">
           <source src="/water.mp4" type="video/mp4" />
         </video>
       </div>
-      <div className='bg-[#1f1f1f] bg-opacity-80 backdrop-filter backdrop-blur-sm w-full h-full absolute top-0 left-0 z-20'>
-        <div className='w-full h-full flex flex-row flex-wrap relative px-5 overflow-hidden justify-between'>
-          <div className='h-full flex items-center absolute'>
-            <DeveloperCard />
-          </div>
-          <div className='h-full overflow-scroll w-full pl-[40%]' id='container' ref={scrollRef}>
-            <Introduction />
-            <motion.div
-              // initial="hidden"
-              // whileInView="visible"
-              variants={container}
-              viewport={{ once: false, amount: 0.8, root: scrollRef }}
-              >
-              <About />
-            </motion.div>
-            <Resume />
-            <Skills />
-            <Portfolio />
-            <Contact />
-          </div>
-          <div className='absolute h-full right-[70px] flex items-center'>
-            <SideNav />
-          </div>
+      <div className='bg-[#1f1f1f] absolute top-0 left-0 w-full h-full bg-opacity-80 -z-20'>
         </div>
+      <div className='h-full flex items-center absolute z-50'>
+        <DeveloperCard />
+      </div>
+      <div className='h-full overflow-scroll w-full pl-[40%] z-50' id='container' ref={scrollRef}>
+        <Introduction />
+        <motion.div
+          // initial="hidden"
+          // whileInView="visible"
+          variants={container}
+          viewport={{ once: false, amount: 0.8, root: scrollRef }}
+        >
+          <About />
+        </motion.div>
+        <Resume />
+        <Skills />
+        <Portfolio />
+        <Contact />
+      </div>
+      <div className='absolute h-full right-[70px] top-0 flex items-center z-50'>
+        <SideNav />
       </div>
     </main>
   )
